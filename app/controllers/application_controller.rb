7cfc75f014ccee3f7fd8d1ece8d6d0d6a59a03c1
hpneo <hpneo@hotmail.com>
2017-11-17 21:52:21 -0500

    Allow CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
-  before_action :allow_cors
-
-  def allow_cors
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-    response.headers['Access-Control-Allow-Methods'] = '*'
-  end
 end
